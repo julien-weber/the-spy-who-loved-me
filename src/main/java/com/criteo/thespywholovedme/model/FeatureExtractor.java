9b46f27189e5fea89d29422b04d530b689b21bec
Jonathan Wu <jo.wu@criteo.com>
2016-04-14 15:13:11 -0700

    add missing FeatureExtractor folders

--- src/main/java/com/criteo/thespywholovedme/model/FeatureExtractor.java
+++ src/main/java/com/criteo/thespywholovedme/model/FeatureExtractor.java
@@ -1,6 +1,5 @@
 package com.criteo.thespywholovedme.model;
 
-
 //import org.apache.axis.utils.StringUtils;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -16,45 +15,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
-public class FeatureExtractor  {
-
+public class FeatureExtractor {
 
     private static RealMatrix US;
     private static String filePath = "src/test/resources/model/featureVectorFile";
-    
 
-    public void  writeFeatures(List<TermIDF> dictionary, List<Map<String,Integer>> posResumes,
-                              List<Map<String,Integer>> negResumes) {
-        String[] outputStringFeatures =  extractStringFeatures(dictionary,  posResumes, negResumes);
-        
+    public void writeFeatures(List<TermIDF> dictionary, List<Map<String, Integer>> posResumes, List<Map<String, Integer>> negResumes) {
+        String[] outputStringFeatures = extractStringFeatures(dictionary, posResumes, negResumes);
+
         writetoFile(filePath, outputStringFeatures);
 
     }
-    
-    public String[]  extractStringFeatures(List<TermIDF> dictionary, List<Map<String,Integer>> posResumes,
-            List<Map<String,Integer>> negResumes) {
-List<List<Double>> X1_N =  extractFeatures(dictionary,  posResumes, negResumes);
-double[][] array = convertListToArray(X1_N);
-RealMatrix vU = SVD.getSVD(array);
 
-   return getFeatureString(vU, posResumes.size() );
+    public String[] extractStringFeatures(List<TermIDF> dictionary, List<Map<String, Integer>> posResumes, List<Map<String, Integer>> negResumes) {
+        List<List<Double>> X1_N = extractFeatures(dictionary, posResumes, negResumes);
+        double[][] array = convertListToArray(X1_N);
+        RealMatrix vU = SVD.getSVD(array);
 
-}
+        return getFeatureString(vU, posResumes.size());
 
+    }
 
-
-    public List<List<Double>> extractFeatures(List<TermIDF> dictionary, List<Map<String,Integer>> posResumes,
-                                              List<Map<String,Integer>> negResumes ) {
+    public List<List<Double>> extractFeatures(List<TermIDF> dictionary, List<Map<String, Integer>> posResumes, List<Map<String, Integer>> negResumes) {
 
         List<List<Double>> X1_N = new ArrayList<List<Double>>();
 
-        for (Map<String, Integer> resume : posResumes) {
+        for (Map<String, Integer> resume: posResumes) {
             List<Double> X = extractFeatures(dictionary, resume);
             X1_N.add(X);
         }
 
-        for (Map<String, Integer> resume : negResumes) {
+        for (Map<String, Integer> resume: negResumes) {
             List<Double> X = extractFeatures(dictionary, resume);
             X1_N.add(X);
         }
@@ -62,25 +53,23 @@ RealMatrix vU = SVD.getSVD(array);
         return X1_N;
     }
 
-    public void writeFeatures(List<TermIDF> dictionary, Map<String,Integer> resume) {
+    public void writeFeatures(List<TermIDF> dictionary, Map<String, Integer> resume) {
         List<Double> X = extractFeatures(dictionary, resume);
 
     }
 
-
-
-    public List<Double> extractFeatures(List<TermIDF> dictionary, Map<String,Integer> resume)  {
+    public List<Double> extractFeatures(List<TermIDF> dictionary, Map<String, Integer> resume) {
 
         List<Double> X = new ArrayList<Double>();
 
         for (TermIDF termIdf: dictionary) {
-                String term = termIdf.getTerm();
-                Integer count = resume.get(term);
-                double tgidf = 0;
-                if (count != null) {
-                    tgidf = count*termIdf.getIdf();
-                }
-                X.add(tgidf);
+            String term = termIdf.getTerm();
+            Integer count = resume.get(term);
+            double tgidf = 0;
+            if (count != null) {
+                tgidf = count * termIdf.getIdf();
+            }
+            X.add(tgidf);
 
         }
 
@@ -93,44 +82,40 @@ RealMatrix vU = SVD.getSVD(array);
         int rowSize = xnlist.size();
         int colSize = xnlist.get(0).size();
         double[][] arr = new double[rowSize][colSize];
-        for ( int i = 0; i < rowSize; i++)  {
-            for (int j =0; j < colSize; j++) {
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
                 arr[i][j] = xnlist.get(i).get(j);
             }
         }
         return arr;
     }
-/*
-    private RealMatrix SVD(double[][] matrixData) {
-        // double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
-        RealMatrix m = MatrixUtils.createRealMatrix(matrixData);
-        SingularValueDecomposition svd = new SingularValueDecomposition(m);
-
-        // compute the singular vallue decomposition
-        RealMatrix U = svd.getU();
-        RealMatrix S = svd.getS();
-        RealMatrix V = svd.getV();
-        US = svd.getU().multiply(S);
-        RealMatrix VS = svd.getV().multiply(S);
-        return VS;
-
-    }
-    
-    */
+    /*
+     * private RealMatrix SVD(double[][] matrixData) { // double[][] matrixData
+     * = { {1d,2d,3d}, {2d,5d,3d}}; RealMatrix m =
+     * MatrixUtils.createRealMatrix(matrixData); SingularValueDecomposition svd
+     * = new SingularValueDecomposition(m);
+     *
+     * // compute the singular vallue decomposition RealMatrix U = svd.getU();
+     * RealMatrix S = svd.getS(); RealMatrix V = svd.getV(); US =
+     * svd.getU().multiply(S); RealMatrix VS = svd.getV().multiply(S); return
+     * VS;
+     *
+     * }
+     *
+     */
 
     private void writetoFile(String filePath, String[] outputFeatures) {
         try {
             File file = new File(filePath);
-
+            file.mkdirs();
 
             // if file doesnt exists, then create it
             if (!file.exists()) {
                 file.createNewFile();
             } else {
-            	file.delete();
-            	file.createNewFile();
+                file.delete();
+                file.createNewFile();
             }
-            	
 
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
@@ -144,37 +129,33 @@ RealMatrix vU = SVD.getSVD(array);
         }
 
     }
-    
-    private String[] getFeatureString(RealMatrix vU,  int posSize) {
-        
-            int cols = vU.getColumnDimension();
-            int rows = vU.getRowDimension();
 
+    private String[] getFeatureString(RealMatrix vU, int posSize) {
 
-            String[] outputFeatures = new String[rows];
+        int cols = vU.getColumnDimension();
+        int rows = vU.getRowDimension();
 
-            for (int i = 0; i < rows; i++) {
-                StringBuilder s = new StringBuilder();
-                for (int j = 0; j < cols; j++) {
-                    double val = vU.getEntry(i,j);
-                    if (j > 0) {
-                        s.append(",");
-                    }
-                    s.append(val);
+        String[] outputFeatures = new String[rows];
+
+        for (int i = 0; i < rows; i++) {
+            StringBuilder s = new StringBuilder();
+            for (int j = 0; j < cols; j++) {
+                double val = vU.getEntry(i, j);
+                if (j > 0) {
+                    s.append(",");
                 }
-                if (i < posSize) {
-                    s.append(",1");
-                } else {
-                    s.append(",0");
-                }
-                outputFeatures[i] = s.toString();
+                s.append(val);
             }
+            if (i < posSize) {
+                s.append(",1");
+            } else {
+                s.append(",0");
+            }
+            outputFeatures[i] = s.toString();
+        }
 
-
-           return outputFeatures;
+        return outputFeatures;
 
     }
-
-
 
 }
