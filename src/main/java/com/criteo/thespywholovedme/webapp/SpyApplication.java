00ddb9d9fecee0ac92e86448e05c65be93069916
Jonathan Wu <jo.wu@criteo.com>
2016-04-14 14:56:07 -0700

    setup index page and initial upload method. also fix maven plugin

--- src/main/java/com/criteo/thespywholovedme/webapp/SpyApplication.java
+++ src/main/java/com/criteo/thespywholovedme/webapp/SpyApplication.java
@@ -1,31 +1,56 @@
 package com.criteo.thespywholovedme.webapp;
 
+import java.io.File;
 import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.criteo.thespywholovedme" })
 public class SpyApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SpyApplication.class);
 
+    public static final String PDF_DIR = "web/pdf-input";
+    public static final String TXT_DIR = "web/txt-output";
+
     /**
      * @param args
      */
     public static void main(String[] args) {
 
-        ApplicationContext ctx = SpringApplication.run(new Object[] { SpyApplication.class }, args);
-        logger.info("beans detected:");
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName: beanNames) {
-            logger.info(beanName);
+        try {
+            ApplicationContext ctx = SpringApplication.run(new Object[] { SpyApplication.class }, args);
+            logger.info("beans detected:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName: beanNames) {
+                logger.info(beanName);
+            }
+        } catch (Throwable e) {
+            logger.error("Error in Application", e);
         }
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return new CommandLineRunner() {
+
+            @Override
+            public void run(String... arg0) throws Exception {
+                new File(PDF_DIR).mkdirs();
+                new File(TXT_DIR).mkdirs();
+
+            }
+        };
     }
 }
--- src/main/java/com/criteo/thespywholovedme/webapp/controller/SpyController.java
+++ src/main/java/com/criteo/thespywholovedme/webapp/controller/SpyController.java
@@ -1,14 +1,42 @@
 package com.criteo.thespywholovedme.webapp.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.criteo.thespywholovedme.webapp.SpyApplication;
 
 @Controller
 public class SpyController {
 
     @RequestMapping("/")
-    public @ResponseBody String home() {
-        return "home";
+    public String home() {
+
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/pdf/upload")
+    public String handlePdfUpload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(SpyApplication.PDF_DIR + "/" + name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+                stream.close();
+                redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + name + "!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message", "You failed to upload " + name + " => " + e.getMessage());
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("message", "You failed to upload " + name + " because the file was empty");
+        }
+        return "";
     }
 }
