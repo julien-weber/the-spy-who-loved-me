91eef9962c0253e22a921bc7b1a8a59a028914b6
Jonathan Wu <jo.wu@criteo.com>
2016-04-14 16:36:24 -0700

    upload form and perform pdf upload

--- src/main/java/com/criteo/thespywholovedme/webapp/controller/SpyController.java
+++ src/main/java/com/criteo/thespywholovedme/webapp/controller/SpyController.java
@@ -4,11 +4,17 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,17 +23,21 @@ import com.criteo.thespywholovedme.webapp.SpyApplication;
 @Controller
 public class SpyController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpyController.class);
+
     @RequestMapping("/")
     public String home() {
 
         return "index";
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/pdf/upload")
-    public String handlePdfUpload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+    @RequestMapping(method = RequestMethod.POST, value = "/upload")
+    public String handlePdfUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        String name = file.getOriginalFilename();
         if (!file.isEmpty()) {
             try {
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(SpyApplication.PDF_DIR + "/" + name)));
+                File uploadedPdf = new File(SpyApplication.PDF_DIR + "/" + name);
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(uploadedPdf));
                 FileCopyUtils.copy(file.getInputStream(), stream);
                 stream.close();
                 redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + name + "!");
@@ -37,6 +47,13 @@ public class SpyController {
         } else {
             redirectAttributes.addFlashAttribute("message", "You failed to upload " + name + " because the file was empty");
         }
-        return "";
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody String handleException(Exception e, HttpServletResponse response) {
+        logger.error("Error caught", e);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return e.getMessage();
     }
 }
