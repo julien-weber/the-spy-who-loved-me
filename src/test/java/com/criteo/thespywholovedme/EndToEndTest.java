0a52cb8c5297e38bfb6dab2bee69d1d8ada5d0e5
Jonathan Wu <jo.wu@criteo.com>
2016-04-14 19:51:35 -0700

    Add final featureVectorFile

--- src/test/java/com/criteo/thespywholovedme/EndToEndTest.java
+++ src/test/java/com/criteo/thespywholovedme/EndToEndTest.java
@@ -5,7 +5,11 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,70 +18,64 @@ import com.criteo.thespywholovedme.prediction.Prediction;
 import com.criteo.thespywholovedme.prediction.PredictionTest;
 import com.criteo.thespywholovedme.tokenizer.Processor;
 import com.criteo.thespywholovedme.tools.PdfToTextService;
+import com.google.common.collect.Lists;
 
 public class EndToEndTest {
 
     static Logger log = LoggerFactory.getLogger(EndToEndTest.class);
 
     @Test
-    public void endToEndTest() {
-        String resumeToPredictStr = "target/txts/NewCadidate.pdf";
+    @Ignore
+    public void training() {
 
-        // Training:
-        Processor processor = Training();
+        String goodResumesPath = "profiles_hired_output";
+        List<File> goodResumesInText = Lists.newArrayList(FileUtils.listFiles(new File(goodResumesPath), FileFilterUtils.trueFileFilter(), FileFilterUtils.directoryFileFilter()));
 
-        // Make a prediction
-        // TODO integrate with ML
-        List<Double> weight = Arrays.asList(0.1, 0.5, 1.0);
-        // List<Double> weight = processor.GetWeight();
-        List<Double> XWithTfIdf = Arrays.asList(1.0, 1.0, 2.0);
-        // File resumeToPredict = new File(resumeToPredictStr);
-        // List<Double> XWithTfIdf = processor.GetXWithTfIdf(resumeToPredict);
-        MakePrediction(weight, XWithTfIdf);
+        String badResumesPath = "profiles_rejected_output";
+        List<File> badResumesInText = Lists.newArrayList(FileUtils.listFiles(new File(badResumesPath), FileFilterUtils.trueFileFilter(), FileFilterUtils.directoryFileFilter()));
+
+        // 2. Tokenizer + ML
+        Processor processor = new Processor();
+        processor.process(goodResumesInText, badResumesInText);
+
     }
 
     @Test
-    public void PredictTest()
-    {
-    	 //String resumeToPredictStr = "target/txts/JonWuTechResume.pdf";
-    	 String resumeToPredictStr = "src/test/resources/pdfs/JonWuTechResume.pdf";
+    public void PredictTest() {
+        // String resumeToPredictStr = "target/txts/JonWuTechResume.pdf";
+        String resumeToPredictStr = "src/test/resources/pdfs/JonWuTechResume.pdf";
 
+        String outputPath = "target/txts";
+        File resumeToPredict = new File(resumeToPredictStr);
+        PdfToTextService pdfToTextService = new PdfToTextService(outputPath);
+        List<File> convertedResumes = pdfToTextService.convert(resumeToPredict);
+        File convertedResume = convertedResumes.get(0);
 
-    	 String outputPath = "target/txts";
-         File resumeToPredict = new File(resumeToPredictStr);
-         PdfToTextService pdfToTextService = new PdfToTextService(outputPath);
-         List<File> convertedResumes = pdfToTextService.convert(resumeToPredict);
-         File convertedResume = convertedResumes.get(0);
+        Processor processor = new Processor();
+        List<Double> weight = Arrays.asList(0.1, 0.5, 1.0);
+        // List<Double> weight = processor.GetWeight();
 
-    	 Processor processor = new Processor();
-    	 List<Double> weight = Arrays.asList(0.1, 0.5, 1.0);
-    	 //List<Double> weight = processor.GetWeight();
+        List<Double> XWithTfIdf = Arrays.asList(1.0, 1.0, 2.0);
+        // List<Double> XWithTfIdf = processor.GetXWithTfIdf(convertedResume);
 
-    	 List<Double> XWithTfIdf = Arrays.asList(1.0, 1.0, 2.0);
-    	 //List<Double> XWithTfIdf = processor.GetXWithTfIdf(convertedResume);
-
-    	 Prediction prediction = Prediction.getInstance();
-         double finalScore = prediction.getFinalScore(weight, XWithTfIdf);
-         log.info("the final score is " + finalScore);
-         if (finalScore > 0.5) {
-             log.info("Candidate is hired");
-         } else {
-             log.info("Candidate is not hired");
-         }
+        Prediction prediction = Prediction.getInstance();
+        double finalScore = prediction.getFinalScore(weight, XWithTfIdf);
+        log.info("the final score is " + finalScore);
+        if (finalScore > 0.5) {
+            log.info("Candidate is hired");
+        } else {
+            log.info("Candidate is not hired");
+        }
 
     }
 
     private Processor Training() {
-        // 1. Convert Files from Pdf to Text
-        String outputPathGoodResumes = "target/txts/positive";
-        File goodResumesPdf = new File("src/test/resources/pdfs/positive");
-        PdfToTextService pdfToTextServiceGoodResumes = new PdfToTextService(outputPathGoodResumes);
-        List<File> goodResumesInText = pdfToTextServiceGoodResumes.convert(goodResumesPdf);
 
-        String outputPathBadResumes = "target/txts/negative";
-        File badResumesPdf = new File("src/test/resources/pdfs/negative");
-        PdfToTextService pdfToTextServiceBadResumes = new PdfToTextService(outputPathBadResumes);
-        List<File> badResumesInText = pdfToTextServiceBadResumes.convert(badResumesPdf);
+        String goodResumesPath = "profiles_hired_output";
+        List<File> goodResumesInText = Lists.newArrayList(FileUtils.listFiles(new File(goodResumesPath), FileFilterUtils.trueFileFilter(), FileFilterUtils.directoryFileFilter()));
+
+        String badResumesPath = "profiles_rejected_output";
+        List<File> badResumesInText = Lists.newArrayList(FileUtils.listFiles(new File(badResumesPath), FileFilterUtils.trueFileFilter(), FileFilterUtils.directoryFileFilter()));
 
         // 2. Tokenizer + ML
         Processor processor = new Processor();
