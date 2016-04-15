08b6d7ba0278f2bc024eaf97bc85606ae6b41aaa
Jonathan Wu <jo.wu@criteo.com>
2016-04-15 08:04:51 -0700

    create utility to save and load RealMatrix

--- src/main/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelper.java
+++ src/main/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelper.java
@@ -3,22 +3,59 @@ package com.criteo.thespywholovedme.tokenizer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.criteo.thespywholovedme.model.SVD;
 import com.criteo.thespywholovedme.model.TermIDF;
+import com.google.common.collect.Lists;
 
 /*
  * for save and load dictionary
  */
 public class DictionaryHelper {
     static Logger log = LoggerFactory.getLogger(DictionaryHelper.class);
+
+    public static RealMatrix loadMatrix(String path) {
+        List<List<Double>> lists = Lists.newArrayList();
+        File file = new File(path);
+        if (!file.exists()) {
+            log.error("ERROR: " + path + " does not exist!");
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                List<Double> doubleLine = parseDoubleArray(currentLine.split(","));
+                lists.add(doubleLine);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        double[][] result = SVD.convertListToArray(lists);
+        return MatrixUtils.createRealMatrix(result);
+    }
+
+    public static List<Double> parseDoubleArray(String[] splitLine) {
+        List<Double> doubleList = Lists.newArrayList();
+        for (String item: splitLine) {
+            doubleList.add(Double.parseDouble(item));
+        }
+
+        return doubleList;
+    }
 
     public static List<TermIDF> Load(String path) {
         List<TermIDF> dict = new ArrayList<TermIDF>();
@@ -27,7 +64,7 @@ public class DictionaryHelper {
             log.error("ERROR: " + path + " does not exist!");
             return dict;
         }
-        
+
         BufferedReader br = null;
         try {
             String sCurrentLine;
@@ -35,8 +72,8 @@ public class DictionaryHelper {
             while ((sCurrentLine = br.readLine()) != null) {
                 String[] termIDFStrs = sCurrentLine.split(":");
                 if (termIDFStrs.length == 2) {
-                  TermIDF termIDF = new TermIDF(termIDFStrs[0], Double.parseDouble(termIDFStrs[1]));
-                  dict.add(termIDF);
+                    TermIDF termIDF = new TermIDF(termIDFStrs[0], Double.parseDouble(termIDFStrs[1]));
+                    dict.add(termIDF);
                 }
             }
         } catch (IOException e) {
@@ -44,51 +81,83 @@ public class DictionaryHelper {
         } finally {
             if (br != null) {
                 try {
-                  br.close();
+                    br.close();
                 } catch (IOException e) {
-                  // TODO Auto-generated catch block
-                  e.printStackTrace();
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
             }
         }
-        
+
         return dict;
     }
 
-    public static void Save(List<TermIDF> dict, String path) {
-      File file = new File(path);
-      if (file.exists()) {
-          if (file.isFile()) {
-              log.info("file " + path + "exists, will be overwrotten.");
-          } else {
-              log.error("ERROR file " + path + "is a folder.");
-              return;
-          }
-      }
+    public static void saveMatrix(RealMatrix realMatrix, String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isFile()) {
+                log.info("file " + path + "exists, will be overwrotten.");
+            } else {
+                log.error("ERROR file " + path + "is a folder.");
+                return;
+            }
+        } else {
+            try {
+                FileUtils.touch(file);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
-      // save to file
-      BufferedWriter output = null;
-      try {
-          output = new BufferedWriter(new FileWriter(file));
-          StringBuilder sb = new StringBuilder();
-          for (TermIDF termIdf : dict) {
-            sb.append(termIdf.getTerm());
-            sb.append(":");
-            sb.append(termIdf.getIdf());
-            sb.append("\n");
-          }
-          output.write(sb.toString());
-      } catch ( IOException e ) {
-          e.printStackTrace();
-      } finally {
-        if ( output != null ) {
-          try {
-            output.close();
-          } catch (IOException e) {
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(file))) {
+            double[][] data = realMatrix.getData();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.length; i++) {
+                sb.append(StringUtils.join(data[i], ','));
+                sb.append("\n");
+            }
+            output.write(sb.toString());
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-          }
         }
-      }
+    }
+
+    public static void Save(List<TermIDF> dict, String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isFile()) {
+                log.info("file " + path + "exists, will be overwrotten.");
+            } else {
+                log.error("ERROR file " + path + "is a folder.");
+                return;
+            }
+        }
+
+        // save to file
+        BufferedWriter output = null;
+        try {
+            output = new BufferedWriter(new FileWriter(file));
+            StringBuilder sb = new StringBuilder();
+            for (TermIDF termIdf: dict) {
+                sb.append(termIdf.getTerm());
+                sb.append(":");
+                sb.append(termIdf.getIdf());
+                sb.append("\n");
+            }
+            output.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/test/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelperTest.java
+++ src/test/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelperTest.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,22 +16,40 @@ import com.criteo.thespywholovedme.model.TermIDF;
 
 public class DictionaryHelperTest {
     static Logger log = LoggerFactory.getLogger(DictionaryHelperTest.class);
+
     @Test
     public void test() {
-      List<TermIDF> dict = new ArrayList<TermIDF> ();
-      dict.add(new TermIDF("A", 1.));
-      dict.add(new TermIDF("B", 0.));
-      String path = "dict.txt";
-      DictionaryHelper.Save(dict, path);
+        List<TermIDF> dict = new ArrayList<TermIDF>();
+        dict.add(new TermIDF("A", 1.));
+        dict.add(new TermIDF("B", 0.));
+        String path = "dict.txt";
+        DictionaryHelper.Save(dict, path);
 
-      List<TermIDF> dict2 = DictionaryHelper.Load(path);
-      File file = new File(path);
-      boolean success = file.delete();
-      log.info("file deleted ?" + success);
+        List<TermIDF> dict2 = DictionaryHelper.Load(path);
+        File file = new File(path);
+        boolean success = file.delete();
+        log.info("file deleted ?" + success);
 
-      assertTrue(dict.size() == dict2.size());
-      for (TermIDF t : dict) {
-          assertTrue(dict2.contains(t));
-      }
+        assertTrue(dict.size() == dict2.size());
+        for (TermIDF t: dict) {
+            assertTrue(dict2.contains(t));
+        }
     }
+
+    @Test
+    public void realMatrixSerializationTest() throws Exception {
+        double[][] input = new double[][] { { 0.1d, 0.2d }, { 1.1d, 1.2d } };
+        String savePath = "target/realMatrices/realMatrix.txt";
+        RealMatrix realMatrix = MatrixUtils.createRealMatrix(input);
+        DictionaryHelper.saveMatrix(realMatrix, savePath);
+
+        RealMatrix readMatrix = DictionaryHelper.loadMatrix(savePath);
+        double[][] output = readMatrix.getData();
+
+        assertEquals(0.1d, output[0][0], 0.01d);
+        assertEquals(0.2d, output[0][1], 0.01d);
+        assertEquals(1.1d, output[1][0], 0.01d);
+        assertEquals(1.2d, output[1][1], 0.01d);
+    }
+
 }
