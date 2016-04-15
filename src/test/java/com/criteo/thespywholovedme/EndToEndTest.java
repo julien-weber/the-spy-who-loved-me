d9d9735adf501a58e5f7f56e507c09c113ff3c5d
Maayan Taragan <m.taragan@criteo.com>
2016-04-14 19:22:53 -0700

    add prediction test

--- src/test/java/com/criteo/thespywholovedme/EndToEndTest.java
+++ src/test/java/com/criteo/thespywholovedme/EndToEndTest.java
@@ -36,6 +36,37 @@ public class EndToEndTest {
         MakePrediction(weight, XWithTfIdf);
     }
 
+    @Test
+    public void PredictTest()
+    {
+    	 //String resumeToPredictStr = "target/txts/JonWuTechResume.pdf";
+    	 String resumeToPredictStr = "src/test/resources/pdfs/JonWuTechResume.pdf";
+
+
+    	 String outputPath = "target/txts";
+         File resumeToPredict = new File(resumeToPredictStr);
+         PdfToTextService pdfToTextService = new PdfToTextService(outputPath);
+         List<File> convertedResumes = pdfToTextService.convert(resumeToPredict);
+         File convertedResume = convertedResumes.get(0);
+
+    	 Processor processor = new Processor();
+    	 List<Double> weight = Arrays.asList(0.1, 0.5, 1.0);
+    	 //List<Double> weight = processor.GetWeight();
+
+    	 List<Double> XWithTfIdf = Arrays.asList(1.0, 1.0, 2.0);
+    	 //List<Double> XWithTfIdf = processor.GetXWithTfIdf(convertedResume);
+
+    	 Prediction prediction = Prediction.getInstance();
+         double finalScore = prediction.getFinalScore(weight, XWithTfIdf);
+         log.info("the final score is " + finalScore);
+         if (finalScore > 0.5) {
+             log.info("Candidate is hired");
+         } else {
+             log.info("Candidate is not hired");
+         }
+
+    }
+
     private Processor Training() {
         // 1. Convert Files from Pdf to Text
         String outputPathGoodResumes = "target/txts/positive";
