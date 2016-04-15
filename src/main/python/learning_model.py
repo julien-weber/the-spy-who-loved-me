c266370f11ae66d8ac093dc7c708a01c01f5638a
protobuf correction <b.behmardi@criteo.com>
2016-04-14 18:15:38 -0700

    add learning model

---
+++ src/main/python/learning_model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import statsmodels.api as sm
+import numpy as np
+
+def learning (filename):
+  data = np.loadtxt(filename, delimiter=',')
+  [nRow,nCol] = data.shape
+  data_train = data[:,0:nCol-2]
+  labels_train = data[:,nCol-1]
+  logit = sm.Logit(labels_train, data_train)
+  result = logit.fit()
+  print result.summary()
+  return result.params
