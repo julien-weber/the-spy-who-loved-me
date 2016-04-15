d4cc992eb045e62f62fe2bfd5292aa88ebe1895f
kaiyu yang <k.yang@criteo.com>
2016-04-14 20:12:15 -0700

    add model dictionary load/save

--- src/main/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelper.java
+++ src/main/java/com/criteo/thespywholovedme/tokenizer/DictionaryHelper.java
@@ -1,17 +1,18 @@
 package com.criteo.thespywholovedme.tokenizer;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.criteo.thespywholovedme.model.TermIDF;
-import com.criteo.thespywholovedme.prediction.Prediction;
 
 /*
  * for save and load dictionary
@@ -20,42 +21,73 @@ public class DictionaryHelper {
     static Logger log = LoggerFactory.getLogger(DictionaryHelper.class);
 
     public static List<TermIDF> Load(String path) {
-        return null;
+        List<TermIDF> dict = new ArrayList<TermIDF>();
+        File file = new File(path);
+        if (!file.exists()) {
+            log.error("ERROR: " + path + " does not exist!");
+            return dict;
+        }
+        
+        BufferedReader br = null;
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(path));
+            while ((sCurrentLine = br.readLine()) != null) {
+                String[] termIDFStrs = sCurrentLine.split(":");
+                if (termIDFStrs.length == 2) {
+                  TermIDF termIDF = new TermIDF(termIDFStrs[0], Double.parseDouble(termIDFStrs[1]));
+                  dict.add(termIDF);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                  br.close();
+                } catch (IOException e) {
+                  // TODO Auto-generated catch block
+                  e.printStackTrace();
+                }
+            }
+        }
+        
+        return dict;
     }
 
-    public static void Save(List<TermIDF> dict, String path) throws IOException {
+    public static void Save(List<TermIDF> dict, String path) {
       File file = new File(path);
-      if (file.isFile()) {
-          if (file.exists()) {
-            log.info("file " + path + "exists, will be overwrotten.");
+      if (file.exists()) {
+          if (file.isFile()) {
+              log.info("file " + path + "exists, will be overwrotten.");
           } else {
-              String parent = file.getParent();
-              File parentFolder = new File(parent);
-              if (!parentFolder.exists()) {
-                  boolean successful = parentFolder.mkdirs();
-                  if (!successful) {
-                    log.error("ERROR: failed trying to create the directory " + parent);
-                    return;
-                  }
-              }
+              log.error("ERROR file " + path + "is a folder.");
+              return;
           }
-      } else {
-          log.error("ERROR: need file instead of folder.");
-          return;
       }
 
       // save to file
       BufferedWriter output = null;
       try {
           output = new BufferedWriter(new FileWriter(file));
+          StringBuilder sb = new StringBuilder();
           for (TermIDF termIdf : dict) {
-            output.write(termIdf.toString());
+            sb.append(termIdf.getTerm());
+            sb.append(":");
+            sb.append(termIdf.getIdf());
+            sb.append("\n");
           }
+          output.write(sb.toString());
       } catch ( IOException e ) {
           e.printStackTrace();
       } finally {
         if ( output != null ) {
-          output.close();
+          try {
+            output.close();
+          } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
         }
       }
     }
