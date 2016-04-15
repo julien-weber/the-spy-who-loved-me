2eea437dc5ea7ac2064b9120b20c428d952640fe
protobuf correction <b.behmardi@criteo.com>
2016-04-14 19:19:02 -0700

    refining learning model

--- src/main/python/learning_model.py
+++ src/main/python/learning_model.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
-
-  data = np.loadtxt(filename, delimiter=',')
-  [nRow,nCol] = data.shape
-  data_train = data[:,0:nCol-2]
-  labels_train = data[:,nCol-1]
-  logit = sm.Logit(labels_train, data_train)
-  result = logit.fit()
-  print result.summary()
-  return result.params
+import sys
+filename = sys.argv[1] 
+data = np.loadtxt(filename, delimiter=',')
+[nRow,nCol] = data.shape
+data_train = data[:,0:nCol-2]
+labels_train = data[:,nCol-1]
+logit = sm.Logit(labels_train, data_train)
+result = logit.fit()
+sys.stdout = open("/Users/b.behmardi/weight.txt", "w")
+print result.params
